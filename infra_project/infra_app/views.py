2f3f4a606312911430522107324a8ea13520662f
artem4es <dragster1001@yandex.ru>
2023-04-30 23:57:31 +0300

    autodeploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Офигенчик, а не деплой!')
 
 
 def second_page(request):
